d1b0a686c1a09ccf3b6fc25fae7fd5d1f1a340f8
jbytecode <mhsatman@gmail.com>
2019-04-19 00:25:09 +0300

    cpm function for critical path method

---
+++ src/cpm.R
@@ -0,0 +1,87 @@
+dt <- data.frame(
+  Activity = c("A", "B", "C", "D", "E", "F", "G"),
+  Dependency = c("-", "-", "A, B", "B,C", "C", "D", "A,B, D "),
+  Duration = c(10, 5, 7, 3, 2, 18, 5),
+  stringsAsFactors =  FALSE
+)
+
+getMaxDuration <- function(dat, i){
+  strActivities <- toString(dat$Dependency[i])
+  listActivities <- unlist(strsplit(strActivities, ","))
+  listActivities <- sapply(listActivities, function(s){
+    return(gsub(" ", "", s, fixed = TRUE))
+  })
+  amax <- 0
+  for (e in listActivities){
+    row.index <- which(dat$Activity == e)
+    current.duration <- dat$end[row.index]
+    if(current.duration > amax){
+      amax <- current.duration
+    }
+  }
+  return(amax)
+}
+
+getPrevious <- function(dat, current){
+  i <- which(dat$Activity == current)
+  strActivities <- toString(dat$Dependency[i])
+  listActivities <- unlist(strsplit(strActivities, ","))
+  listActivities <- sapply(listActivities, function(s){
+    return(gsub(" ", "", s, fixed = TRUE))
+  })
+  amax <- 0
+  anc <- "-"
+  for (e in listActivities){
+    row.index <- which(dat$Activity == e)
+    if(length(row.index) < 1){
+      return("-")
+    }
+    current.duration <- dat$end[row.index]
+    if(current.duration > amax){
+      amax <- current.duration
+      anc <- dat$Activity[row.index]
+    }
+  }
+  return(anc)
+}
+
+CPM <- function(dat){
+  n <- dim(dat)[1]
+  start <- rep(0, n)
+  end <- rep(0, n)
+  dat[["start"]] <- start
+  dat[["end"]] <- end
+  
+  for (i in 1:n){
+    # Activity has no dependency
+    if(dat$Dependency[i] == "-"){
+      dat$start[i] <- 0
+      dat$end[i] <- dat$Duration[i]
+    }else{ # Activity has dependencies
+      amax <- getMaxDuration(dat, i)
+      dat$start[i] <- amax
+      dat$end[i] <- amax + dat$Duration[i]
+    }
+  }
+  
+  # Find Critical Path
+  cp <- c()
+  max.end <- order(dat$end, decreasing = TRUE)[1]
+  current <- dat$Activity[max.end]
+  cp <- c(cp, current)
+  while(TRUE){
+    prev <- getPrevious(dat, current)
+    current <- prev
+    cp <- c(cp, current)
+    if(prev == "-"){
+      break
+    }
+  }
+  result <- list(
+    data = dat,
+    critical.path = cp
+  )
+  return(result)
+}
+
+print(CPM(dt))
