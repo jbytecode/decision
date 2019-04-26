9989ce961cc9bb2336863038760719f128c04ea5
jbytecode <mhsatman@gmail.com>
2019-04-27 01:42:17 +0300

    rock - paper - scissors game test

--- test/test-game.R
+++ test/test-game.R
@@ -17,3 +17,14 @@ test_that("Game",{
   expect_equal(result$p.row, c(0.4666667, 0.5333333, 0.0000000), epsilon)
   expect_equal(result$p.col, c(0.6666667, 0.3333333, 0.0000000), epsilon)
 })
+
+test_that("Game - Rock / Paper / Scissors",{
+  testStatus("Game - Rock / Paper / Scissors")
+  a <- matrix(c(0, -1, 1, 1, 0, -1, -1, 1, 0), nrow = 3, byrow = TRUE)
+  result <- game(a + 2)
+  expect_equal(result$g1, result$g2)
+  expect_equal(result$g1, 2, epsilon)
+  expect_equal(result$p.row, c(0.333, 0.333, 0.333), epsilon)
+  expect_equal(result$p.col, c(0.333, 0.333, 0.333), epsilon)
+})
+
