44d45864f7b962a636c9ecf3a80c87470ec1f86e
jbytecode <mhsatman@gmail.com>
2019-05-02 00:17:32 +0300

    elements in game table are modified if there is a negative term

--- package/decisionR/R/game.R
+++ package/decisionR/R/game.R
@@ -3,6 +3,12 @@ library("Rglpk")
 game <- function(mat){
   m <- dim(mat)[1]
   n <- dim(mat)[2]
+  minmat <- min(mat)
+  modified <- FALSE
+  if(minmat < 0){
+    mat <- mat - minmat
+    modified <- TRUE
+  }
   result1 <- Rglpk_solve_LP(
     obj = rep(1, m),
     mat = t(mat),
@@ -23,6 +29,11 @@ game <- function(mat){
   
   g2 <- 1 / result2$optimum
   p2 <- g2 * result2$solution
+  
+  if(modified){
+    g1 <- g1 - abs(minmat)
+    g2 <- g2 - abs(minmat)
+  }
   return(
     list(
       g1 = g1, 
